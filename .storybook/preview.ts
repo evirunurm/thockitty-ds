3f96fc50fc85ed3778b15d1a8f68935815069b4a
Evelin <teakid21@gmail.com>
2026-03-15 21:41:18 +0100

    chore(storybook): add dark/light background presets

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,13 @@ import '../src/tokens/css/variables.css'
 
 const preview: Preview = {
 	parameters: {
+		backgrounds: {
+			default: 'dark',
+			values: [
+				{ name: 'dark', value: '#0f0f0f' },
+				{ name: 'light', value: '#fdfdfd' },
+			],
+		},
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
